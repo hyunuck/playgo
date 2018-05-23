bcc82df9cc1f4a8fd956c30842be19d20f006f46
hyunuck <gusdnr@gmail.com>
2018-05-23 16:38:25 +0900

    array and slice

---
+++ examples/array/array_test.go
@@ -0,0 +1,22 @@
+package array
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Example() {
+	fruits := [3]string{"사과", "바나나", "토마토"}
+	for _, fruit := range fruits {
+		fmt.Printf("%s는 맛있다.\n", fruit)
+	}
+
+	// Output:
+	// 사과는 맛있다.
+	// 바나나는 맛있다.
+	// 토마토는 맛있다.
+}
+
+func Test_a(t *testing.T) {
+	fmt.Println("Asdf")
+}
